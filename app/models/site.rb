3baf496b0c7a56328fda3bcb7981d8d9b45ed878
renandeandradevaz <renanandrade_rj@hotmail.com>
2014-02-11 16:38:57 -0200

    poijh

--- app/models/site.rb
+++ app/models/site.rb
@@ -25,10 +25,33 @@ class Site
         if texto_somente != site.conteudo
           site.conteudo = texto_somente
           site.save
+          enviar_email site
         end
-
       rescue
       end
     end
   end
+
+  private
+  def self.enviar_email(site)
+
+    ActionMailer::Base.smtp_settings = {
+        :address => "smtp.gmail.com",
+        :domain => "gmail.com",
+        :port => 587,
+        :user_name => "monitoramentodesites@gmail.com",
+        :password => "digdindigdin",
+        :authentication => "plain",
+        :enable_starttls_auto => true
+    }
+
+    ActionMailer::Base.mail(
+        :from => "monitoramentodesites@gmail.com",
+        :to => "renanandrade_rj@hotmail.com",
+        :subject => "Atualizacao no site " << site.url,
+        :body => site.url)
+    .deliver
+
+  end
+
 end
